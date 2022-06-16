78ac889f937fd5d20d1163827e5af6476eb5c4aa
jpdicomo <39575121+jpdicomo@users.noreply.github.com>
2022-06-16 11:33:31 -0300

    Agregadas unas cosas con emotion

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
+/** @jsxImportSource @emotion/react */
 import React, { useEffect, useState } from 'react';
 import { Header } from './components/Header';
 import data from './assets/data.json'
 import List from './components/List';
 import Filter from './components/Filter';
 import {Item} from './types/item';
+import {css} from '@emotion/react'
+import styled from '@emotion/styled';
+
 
 interface FilterContextInterface {
   filters: Array<string>;
@@ -37,9 +41,31 @@ function App() {
     )
     )
   }, [filters])
+
+  type HeadingProps = {
+    bg: string,
+    fg: string,
+  }
+
+  const Heading = styled.h1<HeadingProps>`
+    background-color: ${props=> props.bg};
+    color: ${props => props.fg};
+  `;
+
+
   
   return (
     <FilterContext.Provider value={filterContext}>
+      <Heading bg="#008f68" fg="#fae042">
+        Heading with a green background and yellow text
+      </Heading>
+      <div css={css({
+        margin: 10,
+        padding: 10,
+        backgrounColor: '#f22',
+      })}>
+
+      </div>
       <div className='bg-lightCyanBg min-h-screen flex flex-col items-center' style={{fontSize: "15px", fontFamily:"'League Spartan', sans-serif", fontWeight:"500"}}>
         <Header/>
         {filters.length ? <Filter filters={filters}/>: undefined}
